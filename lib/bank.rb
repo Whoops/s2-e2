20303f474ed79b02ec7f243dea89d1ebd68da343
Walton Hoops <whoops@pinnpointe.com>
2010-10-20 13:02:08 -0600

    Build initial framework

---
+++ lib/bank.rb
@@ -0,0 +1,50 @@
+class Bank
+  def initialize()
+    @accounts={}
+  end
+  
+  def account(name)
+    @accounts[name]
+  end
+  
+  def add_account(acc)
+    @accounts[acc.name]=acc
+  end
+  
+  class Account
+    attr_accessor :name
+    attr_reader :balance
+    
+    def initialize(name, initial_deposit=0)
+      @name=name
+      @balance=initial_deposit
+      @deposit_handlers=[]
+      @withdraw_handlers=[]
+    end
+    
+    def deposit(amount)
+      @balance+=amount
+      @deposit_handlers.each { |handler| handler.call(amount, balance) }
+    end
+    
+    def deposit(amount)
+      @balance-=amount
+      @withdraw_handlers.each { |handler| handler.call(amount, balance) }
+    end
+    
+    def transfer(amount, account)
+      account.deposit(amount)
+      withdraw(amount)
+    end
+      
+    
+    def on_deposit(&block)
+      @deposit_handlers << proc &block
+    end
+    
+    def on_withdraw(&block)
+      @withdraw_handlers << proc &block
+    end
+    
+  end
+end
---
+++ spec/bank_spec.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH << File.join(File.expand_path(File.dirname(__FILE__)),'..','lib')
+require 'bank'
+
+describe Bank do
+  it "should call observer on deposit" do
+    
+  end
+end
